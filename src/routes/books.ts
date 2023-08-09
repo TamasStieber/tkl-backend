cb61c4921030a3c768ee794d4733fe7ef9c7db5d
tamas.stieber <tamas.stieber@thyssenkrupp-automotive.com>
2023-08-09 19:47:06 +0200

    Implement book adding and displaying

--- src/routes/books.ts
+++ src/routes/books.ts
@@ -1,56 +1,53 @@
-import { Router } from 'express';
-import Book from '../models/book';
+import { Router } from "express";
+import Book from "../models/book";
+import { IBook } from "../interfaces/interfaces";
 
 const router = Router();
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const books = await Book.find().sort({ createdAt: -1 });
 
     res.status(200).json(books);
   } catch (error) {
-    next(res.status(500).json({ error: 'Internal server error' }));
+    next(res.status(500).json({ error: "Internal server error" }));
   }
 });
 
-router.post('/', async (req, res, next) => {
-//   const newEssay = JSON.parse(req.body.data) as Interfaces.Essay;
+router.post("/", async (req, res, next) => {
+  const newBook = req.body as IBook;
 
-//   const fileName = req.file?.filename;
-//   newEssay.url = fileName ? fileName : '';
-//   try {
-//     const essay = await Essay.create(newEssay);
-//     res.status(200).json(essay);
-//   } catch (error) {
-//     next(res.status(500).json({ error: 'Internal server error' }));
-//   }
+  try {
+    const book = await Book.create(newBook);
+
+    res.status(201).json(book);
+  } catch {
+    next(res.status(500).json({ error: "Internal server error" }));
+  }
 });
 
-router.put('/:id', async (req, res, next) => {
-//   try {
-//     const essay = await Essay.findByIdAndUpdate(
-//       req.params.id,
-//       { $inc: { openCount: 1 } },
-//       { new: true }
-//     );
-
-//     res.status(200).json(essay);
-//   } catch (error) {
-//     next(res.status(500).json({ error: 'Internal server error' }));
-//   }
+router.put("/:id", async (req, res, next) => {
+  //   try {
+  //     const essay = await Essay.findByIdAndUpdate(
+  //       req.params.id,
+  //       { $inc: { openCount: 1 } },
+  //       { new: true }
+  //     );
+  //     res.status(200).json(essay);
+  //   } catch (error) {
+  //     next(res.status(500).json({ error: 'Internal server error' }));
+  //   }
 });
 
-router.delete('/:id', async (req, res, next) => {
-//   try {
-//     const essayToDelete = await Essay.findByIdAndDelete(req.params.id);
-
-//     const fileToDelete = essaysDir + '/' + essayToDelete?.url;
-//     fs.existsSync(fileToDelete) && fs.unlinkSync(fileToDelete);
-
-//     res.status(200).json(essayToDelete);
-//   } catch (error) {
-//     next(res.status(500).json({ error: 'Internal server error' }));
-//   }
+router.delete("/:id", async (req, res, next) => {
+  //   try {
+  //     const essayToDelete = await Essay.findByIdAndDelete(req.params.id);
+  //     const fileToDelete = essaysDir + '/' + essayToDelete?.url;
+  //     fs.existsSync(fileToDelete) && fs.unlinkSync(fileToDelete);
+  //     res.status(200).json(essayToDelete);
+  //   } catch (error) {
+  //     next(res.status(500).json({ error: 'Internal server error' }));
+  //   }
 });
 
 export default router;
